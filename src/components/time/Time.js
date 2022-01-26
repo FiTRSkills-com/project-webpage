435df0352d59670e59586b1a2e47ae7142406b20
Hersh Nagpal <hershnagpal@gmail.com>
2022-01-26 18:18:18 -0500

    Time Table added

--- src/components/time/Time.js
+++ src/components/time/Time.js
@@ -3,7 +3,7 @@ import './style.css'
 
 export const Time = ({}) => {
     return <div className="time">
-        ¯\_(ツ)_/¯
+        <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vR8Is-gMxPH_WTPbYg_uDLK49rc3zWm8rn5dDg6tv51hPaI_swmDcqWQ8tvfSQ7fU_r3PiNlMRbZx8j/pubhtml?widget=true&amp;headers=false" width="1300" height="1000"></iframe>
     </div>
 
 }
